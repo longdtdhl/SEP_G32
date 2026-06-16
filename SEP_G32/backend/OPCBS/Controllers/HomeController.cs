469b36d2e355e6d3f02b1210d1abb423df52d6db
quanghvhe161464@fpt.edu.vn <hviequang0411@gmail.com>
2026-06-17 00:11:21 +0700

    Create HomeController.cs

---
+++ SEP_G32/backend/OPCBS/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OPCBS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
