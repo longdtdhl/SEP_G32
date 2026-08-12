7c26639b5d9e5d668bf6d49b9efcea019f0f7ec5
longdtdhl <longdtdhl@gmail.com>
2026-08-12 23:52:57 +0700

    update doctor subscription

--- SEP_G32/backend/OPCBS.Web/Pages/Doctor/Subscriptions/Status.cshtml.cs
+++ SEP_G32/backend/OPCBS.Web/Pages/Doctor/Subscriptions/Status.cshtml.cs
@@ -1,19 +1,37 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OPCBS.Web.DTOs;
 using OPCBS.Web.Services;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OPCBS.Web.Pages.Doctor.Subscriptions;
 
 public class StatusModel : PageModel
 {
     private readonly ISubscriptionApiService _api;
-    public StatusModel(ISubscriptionApiService api) => _api = api;
+    private readonly IServicePackageApiService _packageApi;
+
+    public StatusModel(ISubscriptionApiService api, IServicePackageApiService packageApi)
+    {
+        _api = api;
+        _packageApi = packageApi;
+    }
+
     public SubscriptionDto? Subscription { get; set; }
+    public ServicePackageDto? CurrentPackage { get; set; }
     public string? Error { get; set; }
 
     public async Task OnGetAsync()
     {
         var (data, error) = await _api.GetCurrentAsync();
-        Subscription = data; Error = error;
+        Subscription = data;
+        Error = error;
+
+        if (Subscription != null && Subscription.ServicePackageId != Guid.Empty)
+        {
+            var (pkgs, _) = await _packageApi.GetAllAsync();
+            CurrentPackage = pkgs?.FirstOrDefault(p => p.Id == Subscription.ServicePackageId);
+        }
     }
 }
